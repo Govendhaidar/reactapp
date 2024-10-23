b55dd05ac72933d5515e915cab0121f2f9fb2488
Govend <Govendhaidar@hotmail.com>
2024-10-23 12:29:22 +0200

    fixat i headern

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,29 +5,29 @@ const Header = () => {
     <div>
        <header>
 
-<div class="container">
+<div className="container">
     <a id="logo" href="index.html"><img src="Link.svg" alt=""/></a>
 
-    <nav id="main-menu" class="navbar">
-        <a class="nav-link" href="#">features</a>
+    <nav id="main-menu" className="navbar">
+        <a className="nav-link" href="#">features</a>
     </nav>
 
-    <div  id="darkmode-toggle-switch" class="btn-toggle-switch">
-        <span class="label">dark mode</span>
-        <label for="dark mode-switch" class="toggle-switch">
+    <div  id="darkmode-toggle-switch" className="btn-toggle-switch">
+        <span className="label">dark mode</span>
+        <label for="dark mode-switch" className="toggle-switch">
             <input id="dark mode-switch" type="checkbox"/>
-            <span class="slider round"></span>
+            <span className="slider round"></span>
 
         </label>
     </div>
 
-    <a id="auth-signin" href="#" class="btn-primary">
-        <i class="fa-thin fa-user-large"></i>
+    <a id="auth-signin" href="#" className="btn-primary">
+        <i className="fa-thin fa-user-large"></i>
         <span>sign in / up</span>
     </a>
 
-    <button class="btn-mobile">
-        <i class="fa-light fa-bars"></i>
+    <button className="btn-mobile">
+        <i className="fa-light fa-bars"></i>
     </button>
 
 
