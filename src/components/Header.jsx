f4262f7c3693a5c32cd2956768ebf1ae1effabc3
Govend <Govendhaidar@hotmail.com>
2024-10-23 12:21:55 +0200

    gjort klart headern

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,42 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+       <header>
+
+<div class="container">
+    <a id="logo" href="index.html"><img src="Link.svg" alt=""/></a>
+
+    <nav id="main-menu" class="navbar">
+        <a class="nav-link" href="#">features</a>
+    </nav>
+
+    <div  id="darkmode-toggle-switch" class="btn-toggle-switch">
+        <span class="label">dark mode</span>
+        <label for="dark mode-switch" class="toggle-switch">
+            <input id="dark mode-switch" type="checkbox"/>
+            <span class="slider round"></span>
+
+        </label>
+    </div>
+
+    <a id="auth-signin" href="#" class="btn-primary">
+        <i class="fa-thin fa-user-large"></i>
+        <span>sign in / up</span>
+    </a>
+
+    <button class="btn-mobile">
+        <i class="fa-light fa-bars"></i>
+    </button>
+
+
+
+    
+
+</div>
+
+</header>
+    </div>
   )
 }
 
